8a66dcd8cdbdbf8ebe4ed1affe14a9cdbf31ebcd
Kausar Fatima <kosarfatima1044@gmail.com>
2024-08-26 09:23:47 +0500

    added

---
+++ android/app/src/main/kotlin/com/example/chat_app/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.chat_app
+
+import android.os.Bundle
+//import androidx.annotation.NonNull
+import io.flutter.embedding.android.FlutterActivity
+// import io.flutter.embedding.engine.FlutterEngine
+// import io.flutter.plugin.common.MethodChannel
+// import com.google.android.gms.common.ConnectionResult
+// import com.google.android.gms.common.GoogleApiAvailability
+
+class MainActivity : FlutterActivity() {
+    // private val CHANNEL = "fcm_channel"
+
+    // override fun onCreate(savedInstanceState: Bundle?) {
+    //     super.onCreate(savedInstanceState)
+
+    //     // Perform the Google Play services check in onCreate()
+    //     checkGooglePlayServices()
+    // }
+
+    // override fun onResume() {
+    //     super.onResume()
+
+    //     // Perform the Google Play services check in onResume()
+    //     checkGooglePlayServices()
+    // }
+
+    // private fun checkGooglePlayServices() {
+    //     val apiAvailability = GoogleApiAvailability.getInstance()
+    //     val resultCode = apiAvailability.isGooglePlayServicesAvailable(this)
+    //     if (resultCode != ConnectionResult.SUCCESS) {
+    //         // Google Play services is not available, show a dialog to the user or handle it in your own way.
+    //         apiAvailability.makeGooglePlayServicesAvailable(this)
+    //     } else {
+    //         // Google Play services is available, continue with your app's functionality.
+    //     }
+    // }
+
+    // override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+    //     super.configureFlutterEngine(flutterEngine)
+    //     MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
+    //         // Handle method calls from Flutter, if needed.
+    //     }
+    // }
+}
---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:chat_app/screens/auth.dart';
+import 'package:chat_app/screens/chat.dart';
+import 'package:chat_app/screens/splash.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const ChatApp());
+}
+
+class ChatApp extends StatelessWidget {
+  const ChatApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Let us Chat',
+      theme: ThemeData().copyWith(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Color.fromARGB(255, 63, 17, 177),
+        ),
+      ),
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const SplashScreen();
+          }
+          if (snapshot.hasData) {
+            return const ChatScreen();
+          } else {
+            return const AuthScreen();
+          }
+        },
+      ),
+    );
+  }
+}
